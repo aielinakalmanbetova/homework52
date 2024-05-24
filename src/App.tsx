1b41c63aec64563ec3afe25b7af2a9742f4beb77
Aielina Kalmanbetova <aielinakalmanbetova@gmail.com>
2024-05-24 16:30:32 +0600

    добавила checkbox, доработала некоторые нюансы нужно исправить кнопку удалить

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,70 @@ import  {ChangeEvent, useState} from 'react';
 import './App.css'
 import './Task.css'
 
+interface Task {
+  id: string;
+  text: string;
+  completed: boolean;
+}
+
 const App = () => {
   const [task,setTask] = useState('');
-  const [todos, setTodos] = useState([
-    {id: '1', text: 'Приготовить завтрак '},
-    {id: '2', text: 'Отвезти сына в садик '},
-    {id: '3', text: 'Поехать по делам '},
+  const [tasks, setTasks] = useState<Task[]>([
+    {id: '1', text: 'Приготовить завтрак ', completed: false},
+    {id: '2', text: 'Отвезти сына в садик ', completed: false},
+    {id: '3', text: 'Поехать по делам ', completed: false},
   ]);
 
-  const addTask = (newTask: string) => {
-    if (newTask.trim() !== '') {
-      const newTodo = {id: Date.now().toString(), text: newTask};
-      setTodos([...todos, newTodo]);
+  const addTask = () => {
+    if (task.trim() !== '') {
+      const newTask: Task = {id: Date.now().toString(), text: task, completed: false};
+      setTasks([...tasks, newTask]);
       setTask('');
     }
   };
 
   const deleteTask = (id: string) => {
-    const updatedTodos = todos.filter(todo => todo.id ! == id);
-    setTodos(updatedTodos)
+    const updatedTasks = tasks.filter((task) => task.id ! == id);
+    setTasks(updatedTasks)
   };
+
+  const toggleTaskCompletion = (id: string) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? {...task, completed: !task.completed} : task
+    );
+    setTasks(updatedTasks);
+  }
 
   const penChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTask(e.target.value);
   };
 
   const penAdd = () => {
-    addTask(task);
-  }
+    addTask();
+  };
 
   return (
     <>
       <h1>ToDo List</h1>
       <ul className='todos'>
-        {todos.map(todo =>(
-          <li className='task' key={todo.id}>
-            <span>{todo.text}</span>
-            <button onClick={() => deleteTask(todo.id)}>
-              <img src='https://cdn-icons-png.freepik.com/512/3221/3221803.png' alt='Delete'/>
+        {tasks.map((task) =>(
+          <li className={`task ${task.completed ? 'completed' : ''}`} key={task.id}>
+            <input type='checkbox' checked={task.completed} onChange={() => toggleTaskCompletion(task.id)}/>
+            <span>{task.text}</span>
+            <button onClick={() => deleteTask(task.id)}>
+              <img className='img' src="https://cdn-icons-png.freepik.com/512/3221/3221803.png" alt="Delete"/>
             </button>
           </li>
         ))}
       </ul>
-      <input type='text' value={task} onChange={penChange} placeholder={'Ведите задачу'}/>
-      <button className='button' onClick={penAdd}>Add</button>
+      <div>
+        <input
+        type='text'
+        value={task}
+        onChange={penChange}
+        placeholder={'Ведите задачу'}/>
+      </div>
+      <button className="button" onClick={penAdd}>Add</button>
     </>
   );
 };
