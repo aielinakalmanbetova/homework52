1e4073b78d0b61123c9637ebdc2c1bcfced331d1
Aielina Kalmanbetova <aielinakalmanbetova@gmail.com>
2024-05-24 10:49:12 +0600

    Добавлен хук, функция для новой задачи, удаления задачи

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import React, {useState} from 'react';
 import './App.css'
 
 const App = () => {
+  const [task,setTask] = useState('');
+  const [todos, setTodos] = useState([
+    {id: '1', text: 'Приготовить завтрак'},
+    {id: '2', text: 'Отвезти сына в садик'},
+    {id: '3', text: 'Поехать по делам'},
+  ]);
+
+  const addTask = () => {
+    if (task.trim() !== '') {
+      const newTodo = {id: Date.now().toString(), text: task};
+      setTodos([...todos, newTodo]);
+      setTask('');
+    }
+  };
+
+  const deleteTask = (id) => {
+    const updatedTodos = todos.filter(todo => todo.id ! == id);
+    setTodos(updatedTodos)
+  };
+
   return (
     <>
-      <h1>Todo List</h1>
+      <h1>TodoList</h1>
+      <ul className='todos'>
+        {todos.map(todo =>(
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => deleteTask(todo.id)}>Удалить</button>
+          </li>
+          ))}
+      </ul>
+      <input type='text' value={task} onChange={(e) => setTask(e.target.value)} placeholder={'Ведите задачу'}/>
+      <button className='button' onClick={addTask}>Add</button>
     </>
   );
-}
+};
 
-export default App
+export default App;
