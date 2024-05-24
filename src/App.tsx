fd7a229cc70c9abdd4535f4d053db6e8d7bc6e54
Aielina Kalmanbetova <aielinakalmanbetova@gmail.com>
2024-05-24 15:49:40 +0600

    реализация стили,

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,51 @@
-import React, {useState} from 'react';
+import  {ChangeEvent, useState} from 'react';
 import './App.css'
+import './Task.css'
 
 const App = () => {
   const [task,setTask] = useState('');
   const [todos, setTodos] = useState([
-    {id: '1', text: 'Приготовить завтрак'},
-    {id: '2', text: 'Отвезти сына в садик'},
-    {id: '3', text: 'Поехать по делам'},
+    {id: '1', text: 'Приготовить завтрак '},
+    {id: '2', text: 'Отвезти сына в садик '},
+    {id: '3', text: 'Поехать по делам '},
   ]);
 
-  const addTask = () => {
-    if (task.trim() !== '') {
-      const newTodo = {id: Date.now().toString(), text: task};
+  const addTask = (newTask: string) => {
+    if (newTask.trim() !== '') {
+      const newTodo = {id: Date.now().toString(), text: newTask};
       setTodos([...todos, newTodo]);
       setTask('');
     }
   };
 
-  const deleteTask = (id) => {
+  const deleteTask = (id: string) => {
     const updatedTodos = todos.filter(todo => todo.id ! == id);
     setTodos(updatedTodos)
   };
 
+  const penChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value);
+  };
+
+  const penAdd = () => {
+    addTask(task);
+  }
+
   return (
     <>
-      <h1>TodoList</h1>
+      <h1>ToDo List</h1>
       <ul className='todos'>
         {todos.map(todo =>(
-          <li key={todo.id}>
-            {todo.text}
-            <button onClick={() => deleteTask(todo.id)}>Удалить</button>
+          <li className='task' key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={() => deleteTask(todo.id)}>
+              <img src='https://cdn-icons-png.freepik.com/512/3221/3221803.png' alt='Delete'/>
+            </button>
           </li>
-          ))}
+        ))}
       </ul>
-      <input type='text' value={task} onChange={(e) => setTask(e.target.value)} placeholder={'Ведите задачу'}/>
-      <button className='button' onClick={addTask}>Add</button>
+      <input type='text' value={task} onChange={penChange} placeholder={'Ведите задачу'}/>
+      <button className='button' onClick={penAdd}>Add</button>
     </>
   );
 };
